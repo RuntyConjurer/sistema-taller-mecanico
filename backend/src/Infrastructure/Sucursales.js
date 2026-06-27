f08b50c8220d2966b27208bd4b1c81dabfef3b95
RuntyConjurer <169057857+RuntyConjurer@users.noreply.github.com>
2026-06-27 01:01:51 -0400

    modelosucursales

---
+++ backend/src/Infrastructure/Sucursales.js
@@ -0,0 +1,41 @@
+const {DataTypes} = require ('sequelize');
+const sequelize = require('./Database/db');
+
+const Sucursales = sequelize.define('Sucursales', {
+    id_sucursal:{
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true, 
+    },
+    nombre:{
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    direccion:{
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    telefono:{
+        type: DataTypes.STRING(20),
+        allowNull: true,
+    },
+    email:{
+        type: DataTypes.STRING(120),
+        allowNull: true,
+    },
+    activa:{
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+    },
+    creado_en:{
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+        
+},
+{
+    tableName: 'sucursales',
+    timestamps: false,
+})
